3b3882c97074ec96e4fb005c434063c4613c824d
Roman Sofin <roma.sofin@gmail.com>
2024-01-22 09:45:13 +0300

    Add macros and clippy exercises

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -5,14 +5,21 @@
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
+// use self::macros::{my_macro};
+use self::macros::my_macro;
+// use macros::my_macro as my_macro;
+
+// #[macro_export]
 mod macros {
+    // extern mod macros;
+
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
     }
+    pub(crate) use my_macro;
 }
 
 fn main() {
